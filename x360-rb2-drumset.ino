90416a17877004d52e098567a08479524138ff69
alecbiela <alec.bielanos@gmail.com>
2024-02-16 10:52:53 -0500

    Remove MIDI, Add Cymbal Detection

--- x360-rb2-drumset.ino
+++ x360-rb2-drumset.ino
@@ -1,9 +1,8 @@
 /** 
  * Rock Band 2 to PC Converter
  * Adapted From https://github.com/evankale/ArduinoMidiDrums
- * I'm really only using the hit-detection code for now, but leaving
- * the MIDI stuff in there for future additions (such as adapting for console use?)
- * Having it commented will exclude it at compile-time, so no harm done there
+ * Changed A LOT of code to reflect the reverse (pulling down inputs instead of peaking up) firing style
+ * and removed everything to do with MIDI but the original concept of how this works is the same.
 **/
 
 // Lots of defines and variables
@@ -11,62 +10,29 @@
 #define NUM_FOOT_SWITCHES 2    // Number of Kick Pedals
 
 #define DEBUG true             // Enables Serial.println messages
-#define MIDI false             // Enables MIDI-out
-#define PC_OUT true            // Enables Joystick-out (PC)
 
 #define SNARE_THRESHOLD 200    // Red Pad
 #define HTOM_THRESHOLD 200     // Yellow Pad
 #define MTOM_THRESHOLD 200     // Blue Pad
 #define LTOM_THRESHOLD 200     // Green Pad
 // ---------------------------
-#define HIHAT_THRESHOLD 100    // Yellow Cymbal
-#define RIDECYM_THRESHOLD 250  // Blue Cymbal
-#define CRASHCYM_THRESHOLD 250 // Green Cymbal
+#define HIHAT_THRESHOLD 50    // Yellow Cymbal
+#define RIDECYM_THRESHOLD 50  // Blue Cymbal
+#define CRASHCYM_THRESHOLD 85 // Green Cymbal
 
 // ===========================
 #define PEDAL_THRESHOLD 50     // Both Kicks (shouldn't be needed as it's a digital signal)
 
-
-//MIDI note defines for each trigger
-/* #define SNARE_NOTE 38
-#define HTOM_NOTE 48
-#define MTOM_NOTE 47
-#define LTOM_NOTE 45
-#define HIHAT_NOTE 42
-#define RIDECYM_NOTE 51
-#define CRASHCYM_NOTE 49
-#define KICK_NOTE 36
-#define ALTKICK_NOTE 36
-#define HIHAT_CHOKE_NOTE 78
-
-// Rock Band Midi Adapter notes
-#define SNARE_NOTE 38;  // Midi value for Red Pad
-#define HIHAT_NOTE 22;  // Midi value for Yellow Cymbal
-#define HTOM_NOTE 48;  // Midi value for Yellow Pad
-#define RIDECYM_NOTE 51;  // Midi value for Blue Cymbal
-#define MTOM_NOTE 45;  // Midi value for Blue Pad
-#define CRASHCYM_NOTE 49;  // Midi value for Green Cymbal
-#define LTOM_NOTE 41;  // Midi value for Green pad
-#define KICK_NOTE 33;  // Midi value for Kick pedal
-
-//MIDI defines
-#define NOTE_ON_CMD 0x90
-#define NOTE_OFF_CMD 0x80
-*/
-#define MAX_MIDI_VELOCITY 127
-
-//MIDI baud rate
-//#define SERIAL_RATE 31250
-
 //Pad sensing defines
 //ALL TIME MEASURED IN MILLISECONDS
 #define SIGNAL_BUFFER_SIZE 100
+#define CYMBAL_BUFFER_SIZE 100
 #define PEAK_BUFFER_SIZE 30
 #define MAX_TIME_BETWEEN_PEAKS 20
-#define MIN_TIME_BETWEEN_NOTES 50
+#define MIN_TIME_BETWEEN_NOTES 30
 
 //Cymbal shifting variables
-#define CYMBAL_SHIFT_PIN 5
+#define CYMBAL_SHIFT_PIN A5
 
 //map that holds the mux slots of the piezos
 unsigned short slotMap[NUM_PIEZOS] = {A1, A2, A3, A4};
@@ -76,31 +42,33 @@ unsigned short noteMap[NUM_PIEZOS];
 
 //map that holds the respective threshold to each piezo
 unsigned short thresholdMap[NUM_PIEZOS];
+unsigned short cymThresholdMap[NUM_PIEZOS];
 
-// Ring buffers to store analog signal and peaks
+// Circular buffers to store analog signal and peaks
 short currentSignalIndex[NUM_PIEZOS];
 short currentPeakIndex[NUM_PIEZOS];
+short currentCymbalBufferIndex;
 unsigned short signalBuffer[NUM_PIEZOS][SIGNAL_BUFFER_SIZE];
 unsigned short peakBuffer[NUM_PIEZOS][PEAK_BUFFER_SIZE];
-
+unsigned short cymbalBuffer[CYMBAL_BUFFER_SIZE];
 
 boolean noteReady[NUM_PIEZOS];
-unsigned short noteReadyVelocity[NUM_PIEZOS];
-boolean isLastPeakZeroed[NUM_PIEZOS];
+unsigned short cymbalReadyValue[NUM_PIEZOS];
+boolean isPadIdle[NUM_PIEZOS];
 unsigned long lastPeakTime[NUM_PIEZOS];
 unsigned long lastNoteTime[NUM_PIEZOS];
 
 // Buffers for foot pedals
 // Needed to separate this from other sensors since Rock Band has binary pedals (it's either on or off)
 short currentFootSwitchIndex[NUM_FOOT_SWITCHES];
-unsigned short footSwitchSlotMap[NUM_FOOT_SWITCHES] = {A0, A5}; //A0 and A5 being used as digital for now
+unsigned short footSwitchSlotMap[NUM_FOOT_SWITCHES] = {A0, 5}; //A0 and 5 being used as digital for now
 unsigned short footSwitchNoteMap[NUM_FOOT_SWITCHES];
 unsigned short footSwitchThresholdMap[NUM_FOOT_SWITCHES];
 
 boolean footNoteReady[NUM_FOOT_SWITCHES];
 int footSwitchLastNote[NUM_FOOT_SWITCHES];
 
-uint8_t lastCShift = LOW;
+String colors[NUM_PIEZOS] = {"Blue", "Yellow", "Red", "Green"};
 
 /**
  * The setup function:
@@ -115,23 +83,19 @@ void setup() {
   // Debug Serial Monitoring
   if (DEBUG) {
     Serial.begin(9600);
-    while(!Serial) ;  // makes the program wait until the serial monitor is open
-  }
-
-  if(PC_OUT){
-    //stub for now
-  }
+    // makes the program wait until the serial monitor is open
+    // IMPORTANT: make sure you flag debug as "false" before normal use
+    while(!Serial) ;  
 
   if(DEBUG) Serial.print("Setting up pads on pins ");
   // Init sensor array
   for (byte i=0; i<NUM_PIEZOS; ++i) {
     currentSignalIndex[i] = 0;
     currentPeakIndex[i] = 0;
-    memset(signalBuffer[i],0,sizeof(signalBuffer[i]));
-    memset(peakBuffer[i],0,sizeof(peakBuffer[i]));
+    memset(signalBuffer[i],0xFF,sizeof(signalBuffer[i]));
+    memset(peakBuffer[i],0xFF,sizeof(peakBuffer[i]));
     noteReady[i] = false;
-    noteReadyVelocity[i] = 0;
-    isLastPeakZeroed[i] = true;
+    isPadIdle[i] = true;
     lastPeakTime[i] = 0;
     lastNoteTime[i] = 0;
     Serial.print(slotMap[i]); Serial.print(", ");
@@ -149,44 +113,37 @@ void setup() {
   }
   Serial.println("");
 
-  thresholdMap[0] = SNARE_THRESHOLD;
+
+  thresholdMap[0] = MTOM_THRESHOLD;
   thresholdMap[1] = HTOM_THRESHOLD;
-  thresholdMap[2] = MTOM_THRESHOLD;
+  thresholdMap[2] = SNARE_THRESHOLD;
   thresholdMap[3] = LTOM_THRESHOLD;
-  thresholdMap[4] = HIHAT_THRESHOLD;
-  thresholdMap[5] = RIDECYM_THRESHOLD;
-  thresholdMap[6] = CRASHCYM_THRESHOLD;
+
+  cymThresholdMap[0] = RIDECYM_THRESHOLD;
+  cymThresholdMap[1] = HIHAT_THRESHOLD;
+  cymThresholdMap[2] = 0;
+  cymThresholdMap[3] = CRASHCYM_THRESHOLD;
 
   footSwitchThresholdMap[0] = PEDAL_THRESHOLD;
   footSwitchThresholdMap[1] = PEDAL_THRESHOLD;
 
-  if(MIDI){
-    /*noteMap[0] = SNARE_NOTE;
-    noteMap[1] = HTOM_NOTE;
-    noteMap[2] = MTOM_NOTE;
-    noteMap[3] = LTOM_NOTE;
-    noteMap[4] = HIHAT_NOTE;
-    noteMap[5] = RIDECYM_NOTE;  
-    noteMap[6] = CRASHCYM_NOTE;
-    footSwitchNoteMap[0] = KICK_NOTE;
-    footSwitchNoteMap[1] = ALTKICK_NOTE;*/
-  } else if(PC_OUT){
-    //set up joystick button outputs here?
-    noteMap[0] = 0;
-    noteMap[1] = 1;
-    noteMap[2] = 2;
-    noteMap[3] = 3;
-    noteMap[4] = 4;
-    noteMap[5] = 5;  
-    noteMap[6] = 6;
-    footSwitchNoteMap[0] = 7;
-    footSwitchNoteMap[1] = 8;
-  }
+
+  noteMap[0] = 0;
+  noteMap[1] = 1;
+  noteMap[2] = 2;
+  noteMap[3] = 3;
+  noteMap[4] = 4;
+  noteMap[5] = 5;  
+  noteMap[6] = 6;
+  footSwitchNoteMap[0] = 7;
+  footSwitchNoteMap[1] = 8;
 
   // TODO: Set up control button outputs here
 
   // Set up Cymbal Shifting
-  pinMode(CYMBAL_SHIFT_PIN, INPUT_PULLUP);
+  memset(cymbalBuffer,0xFF,sizeof(signalBuffer));
+  memset(cymbalReadyValue,0xFF,sizeof(cymbalReadyValue));
+  //pinMode(CYMBAL_SHIFT_PIN, INPUT_PULLUP);
 
   // Temporary Setup for LEDs
   // Just want to turn one on so I know when it's plugged in
@@ -198,7 +155,7 @@ void setup() {
   digitalWrite(9, HIGH);
   digitalWrite(10, HIGH);
   digitalWrite(11, HIGH);
-  
+
 }
 
 /**
@@ -207,32 +164,52 @@ void setup() {
  */
 void loop() {
   
-  unsigned long currentTime = millis();
+  //int cym = analogRead(A5);
+  //Serial.println(cym);
 
+  updatePiezos();
+  updateFootswitches();
+
+  //TODO: update regular buttons (dpad, start/select, etc)
+}
+
+void updatePiezos() {
+  unsigned long currentTime;
+  unsigned short newSignal;
+  
   // update the piezo buffers
   // the inner function recordNewPeak() will fire a note if appropriate
   for (byte i=0; i<NUM_PIEZOS; ++i) {
     //get a new signal from analog read
-    unsigned short newSignal = analogRead(slotMap[i]);
+    currentTime = millis();
+    newSignal = analogRead(slotMap[i]);
+    //example: pad 1
+    // update the signal buffer for pad 1 at whatever the current signal index is for pad 1 to the new signal
     signalBuffer[i][currentSignalIndex[i]] = newSignal;
     
-    //if new signal is 0
+    //if new signal is below the hit threshold for this sensor
     if (newSignal < thresholdMap[i]) {
-      if (!isLastPeakZeroed[i] && (currentTime - lastPeakTime[i]) > MAX_TIME_BETWEEN_PEAKS) {
-        recordNewPeak(i,0);
+      //if the pad is not idle but enough time has passed, set it back to idle
+      if (!isPadIdle[i] && ((currentTime - lastPeakTime[i]) > MAX_TIME_BETWEEN_PEAKS)) {
+        recordNewPeak(i,1024);
       } else {
-        //get previous signal
+        //this chunk basically just gets the strongest hit that has been made on the pad within the stored information
+        //not actually sure if this is needed since we're not doing velocity
+        
         short prevSignalIndex = currentSignalIndex[i]-1;
-        if(prevSignalIndex < 0) prevSignalIndex = SIGNAL_BUFFER_SIZE-1;        
+        if(prevSignalIndex < 0) prevSignalIndex = SIGNAL_BUFFER_SIZE-1;
+           
+        //get the previous signal on the signal buffer
         unsigned short prevSignal = signalBuffer[i][prevSignalIndex];
         
-        unsigned short newPeak = 0;
+        unsigned short newPeak = 1024;
         
-        //find the wave peak if previous signal was not 0 by going
-        //through previous signal values until another 0 is reached
-        while (prevSignal >= thresholdMap[i]) {
-          if(signalBuffer[i][prevSignalIndex] > newPeak) {
-            newPeak = signalBuffer[i][prevSignalIndex];        
+        //loop while the previous signal is a hit
+        while (prevSignal <= thresholdMap[i]) {
+          
+          //if the previous signal in the buffer is stronger than the stored strongest, it is now the new strongest
+          if(signalBuffer[i][prevSignalIndex] < newPeak) {
+            newPeak = signalBuffer[i][prevSignalIndex];
           }
           
           //decrement previous signal index, and get previous signal
@@ -240,18 +217,23 @@ void loop() {
           if (prevSignalIndex < 0) prevSignalIndex = SIGNAL_BUFFER_SIZE-1;
           prevSignal = signalBuffer[i][prevSignalIndex];
         }
-        
-        if(newPeak > 0) {
+
+        if(newPeak < 1024) {
           recordNewPeak(i, newPeak);
         }
       }
-  
+
+    //if the pad falls back out of the threshold before time, reset it to idle
+    } else if(!isPadIdle[i] && (newSignal > thresholdMap[i])){
+      recordNewPeak(i,1024);
     }
         
     currentSignalIndex[i]++;
     if (currentSignalIndex[i] == SIGNAL_BUFFER_SIZE) currentSignalIndex[i] = 0;
   }
+}
 
+void updateFootswitches(){
   // update foot switches
   // directly fires presses if the kick is considered "on"
   for (short i=0; i<NUM_FOOT_SWITCHES; ++i) {
@@ -260,123 +242,84 @@ void loop() {
 
     if ( ( dSignal == LOW  ) && (footSwitchLastNote[i] != LOW) ) {
         footSwitchLastNote[i] = dSignal;
-        //midiNoteOn(footSwitchNoteMap[i], MAX_MIDI_VELOCITY);
-        if(PC_OUT) digitalWrite(footSwitchNoteMap[i], LOW);
+         digitalWrite(footSwitchNoteMap[i], LOW);
         if(DEBUG) Serial.println("on!");
     } else if ( ( dSignal == HIGH ) && ( footSwitchLastNote[i] != HIGH ) ) {
         footSwitchLastNote[i] = dSignal;
-        //midiNoteOff(footSwitchNoteMap[i], 0);
         if(DEBUG) Serial.println("off!");
-        if(PC_OUT) digitalWrite(footSwitchNoteMap[i], HIGH);
+        digitalWrite(footSwitchNoteMap[i], HIGH);
     }
   }
-
-  // TESTING: WILL MOVE - read the cymbal shift
-  unsigned short cShift = digitalRead(5);
-
-  if ( ( cShift == LOW  ) && (lastCShift != LOW) ) {
-      //if(PC_OUT) digitalWrite(footSwitchNoteMap[i], LOW);
-      if(DEBUG) Serial.println("Cymbal Time!");
-  } else if ( ( cShift == HIGH ) && ( lastCShift != HIGH ) ) {
-      if(DEBUG) Serial.println("Cymbal Time is Over!");
-      //if(PC_OUT) digitalWrite(footSwitchNoteMap[i], HIGH);
-  }
-  lastCShift = cShift;
 }
 
 void recordNewPeak(short slot, short newPeak) {
-  isLastPeakZeroed[slot] = (newPeak == 0);
+  isPadIdle[slot] = (newPeak == 1024);
   
   unsigned long currentTime = millis();
   lastPeakTime[slot] = currentTime;
   
-  //new peak recorded (newPeak)
+  //put this new peak into the peak buffer
   peakBuffer[slot][currentPeakIndex[slot]] = newPeak;
   
   //1 of 3 cases can happen:
-  // 1) note ready - if new peak >= previous peak
-  // 2) note fire - if new peak < previous peak and previous peak was a note ready
-  // 3) no note - if new peak < previous peak and previous peak was NOT note ready
+  // 1) note becomes ready to fire - if new peak is stronger than the previous peak (and the minimum time between notes has passed)
+  // 2) note fire - if new peak is weaker than the previous, and the note is ready to fire
+  // 3) no note - if new peak is weaker than the previous, and the note is NOT ready to fire
   
   //get previous peak
   short prevPeakIndex = currentPeakIndex[slot]-1;
   if(prevPeakIndex < 0) prevPeakIndex = PEAK_BUFFER_SIZE-1;        
   unsigned short prevPeak = peakBuffer[slot][prevPeakIndex];
-   
-  if (newPeak > prevPeak && (currentTime - lastNoteTime[slot])>MIN_TIME_BETWEEN_NOTES) {
+  
+  //case 1
+  if ((newPeak < prevPeak) && ((currentTime - lastNoteTime[slot]) > MIN_TIME_BETWEEN_NOTES) && (!noteReady[slot])) {
     noteReady[slot] = true;
-    if (newPeak > noteReadyVelocity[slot])
-      noteReadyVelocity[slot] = newPeak;
-  } else if (newPeak < prevPeak && noteReady[slot]) {
-    // skip the calculation if we're just outputting to PC, since it's just a button press
-    if(MIDI) noteFire(noteMap[slot], scaleVelocity(noteReadyVelocity[slot], thresholdMap[slot]));
-    else noteFire(noteMap[slot], 127);
+    cymbalReadyValue[slot] = analogRead(A5);
+    Serial.print("Note at "); Serial.print(slot); Serial.print(" is ready, cymbal is at "); Serial.println(cymbalReadyValue[slot]);
+  //case 2
+  } else if (newPeak > prevPeak && noteReady[slot]) {
+    noteFire(slot);
     noteReady[slot] = false;
-    noteReadyVelocity[slot] = 0;
     lastNoteTime[slot] = currentTime;
   }
-  
+
+  //increment the peak buffer for this note slot
   currentPeakIndex[slot]++;
   if(currentPeakIndex[slot] == PEAK_BUFFER_SIZE) currentPeakIndex[slot] = 0;  
 }
 
-// Normalizes the raw input power to MIDI velocity (0-127)
-unsigned short scaleVelocity(unsigned short originalVelocity, unsigned short threshold) {
-  unsigned short r_min = threshold;
-  unsigned short r_max = 1023;
-  unsigned short t_min = 1;
-  unsigned short t_max = MAX_MIDI_VELOCITY;
+// Fires a note
+void noteFire(byte n) {
+  // we need another array to track note fire times. sometimes it doesn't pick up the press if it's too quick
+  rbNoteOn(n);
+  rbNoteOff(n);
 
-  float value = ((float) t_max - (float) t_min) * ((float) originalVelocity - (float) r_min) / ((float) r_max - (float) r_min) + (float) t_min;
-  return static_cast<unsigned short>(value);
-}
+  // find the index with the strongest cymbal hit
+  unsigned short cymIndex = 10;
+  unsigned short cymStrength = 65535;
 
-// Fires a note - either outputs MIDI or presses a joystick button
-void noteFire(unsigned short note, unsigned short velocity) {
-  if(velocity > MAX_MIDI_VELOCITY)
-    velocity = MAX_MIDI_VELOCITY;
-  
-  if(MIDI){
-    midiNoteOn(note, velocity);
-    midiNoteOff(note, velocity);
-  } else if(PC_OUT){
-    // we need another array to track note fire times. sometimes it doesn't pick up the press if it's too quick
-    rbNoteOn(note);
-    rbNoteOff(note);
+  for(byte i = 0; i<NUM_PIEZOS; ++i){
+    if(cymbalReadyValue[i] < cymStrength){
+      cymStrength = cymbalReadyValue[i];
+      cymIndex = i;
+    }
   }
 
-  if (DEBUG) {
-    Serial.print("Firing note "); Serial.print(note); Serial.print(" at "); Serial.println(velocity);
-    Serial.println(digitalRead(5));
+  // if this pad has the strongest cymbal strength, hit a cymbal, then wipe the array
+  if((cymIndex == n) && (cymStrength < cymThresholdMap[n])){
+    Serial.print("Firing CYMBAL "); Serial.println(colors[n]);
+    memset(cymbalReadyValue,0xFF,sizeof(cymbalReadyValue));
+  } else{
+    Serial.print("Firing PAD "); Serial.println(colors[n]);
   }
 }
 
-void rbNoteOn(byte note){
+void rbNoteOn(byte n){
     //Serial.print("RB On: ");
     //Serial.println(note);
 }
 
-void rbNoteOff(byte note){
+void rbNoteOff(byte n){
     //Serial.print("RB Off: ");
     //Serial.println(note);
-}
-
-void midiNoteOn(byte note, byte midiVelocity) {
-  /*Serial1.write(NOTE_ON_CMD);
-  Serial1.write(note);
-  Serial1.write(midiVelocity);
-  Serial2.write(NOTE_ON_CMD);
-  Serial2.write(note);
-  Serial2.write(midiVelocity);
-  */
-}
-
-void midiNoteOff(byte note, byte midiVelocity) {
-  /*Serial1.write(NOTE_OFF_CMD);
-  Serial1.write(note);
-  Serial1.write(midiVelocity);
-  Serial2.write(NOTE_OFF_CMD);
-  Serial2.write(note);
-  Serial2.write(midiVelocity);
-  }*/
 }
